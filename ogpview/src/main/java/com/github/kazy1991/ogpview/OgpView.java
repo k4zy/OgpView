e766aa5efc92ea8f34e754c9c2f38b26b87af6a0
kazuki-yoshida <kzk.yshd@gmail.com>
2017-01-19 23:57:26 +0900

    Load url with primitive logic

--- ogpview/src/main/java/com/github/kazy1991/ogpview/OgpView.java
+++ ogpview/src/main/java/com/github/kazy1991/ogpview/OgpView.java
@@ -5,8 +5,21 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class OgpView extends FrameLayout {
     public OgpView(Context context, AttributeSet attrs) {
@@ -18,5 +31,50 @@ public class OgpView extends FrameLayout {
         View.inflate(getContext(), R.layout.ogp_view, this);
         ((SimpleDraweeView) findViewById(R.id.favicon)).setImageURI("http://ogp.me/logo.png");
         ((SimpleDraweeView) findViewById(R.id.og_image)).setImageURI("http://ogp.me/logo.png");
+    }
+
+    public void loadUrl(String url) {
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .readTimeout(15 * 1000, TimeUnit.MILLISECONDS)
+                .writeTimeout(20 * 1000, TimeUnit.MILLISECONDS)
+                .connectTimeout(20 * 1000, TimeUnit.MILLISECONDS)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                try {
+                    String html = response.body().string();
+                    Document document = Jsoup.parse(html);
+                    final String title = document.title();
+                    final String ogTitle = document.select("meta[property=og:title]").attr("content");
+                    final String ogImage = document.select("meta[property=og:image]").attr("content");
+                    final String ogDescription = document.select("meta[property=og:description]").attr("content");
+                    post(new Runnable() {
+                        @Override
+                        public void run() {
+                            ((TextView) findViewById(R.id.site_title)).setText(title);
+                            ((TextView) findViewById(R.id.og_title)).setText(ogTitle);
+                            ((TextView) findViewById(R.id.og_description)).setText(ogDescription);
+                            ((SimpleDraweeView) findViewById(R.id.favicon)).setImageURI(ogImage);
+                            ((SimpleDraweeView) findViewById(R.id.og_image)).setImageURI(ogImage);
+                        }
+                    });
+                } catch (IOException e) {
+                    // todo: error handling
+                }
+            }
+        });
+
     }
 }
--- sample/src/main/java/com/github/kazy1991/ogpview/sample/MainActivity.java
+++ sample/src/main/java/com/github/kazy1991/ogpview/sample/MainActivity.java
@@ -1,7 +1,9 @@
 package com.github.kazy1991.ogpview.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.kazy1991.ogpview.OgpView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((OgpView) findViewById(R.id.ogp_view)).loadUrl("http://ogp.me/");
     }
 }
